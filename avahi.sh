a644e2d6586b06ef87eb9b54a6e0eacdaf71e083
Vogler K. <151208789+Vogler-K@users.noreply.github.com>
2025-01-03 20:25:14 +0100

    Update avahi.sh

--- avahi.sh
+++ avahi.sh
@@ -1,1 +1,1 @@
-sudo apt-get install cmake sudo apt-get install libavahi-compat-libdnssd-dev sudo apt-get install libplist-dev sudo apt-get install libssl-dev sudo apt-get isntall git
+sudo apt-get install cmake libavahi-compat-libdnssd-dev libplist-dev libssl-dev
